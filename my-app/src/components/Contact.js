07fb38e2989d0c6e318004da4d9870b4b8ff0444
Eric Emery <ericemery099@gmail.com>
2023-09-05 17:29:16 +0000

    email form update

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -2,6 +2,22 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <section>
+      <div className="container">
+        <h2 className="text-center">Contact Us</h2>
+          <form>
+            <input type="text"
+            placeholder='Full Name'
+            name='user_name' />
+            <input type="email"
+            placeholder='Email'
+            name='user_email' required />
+            <input type="text"
+            placeholder='Subject'
+            name='subject' required />
+            <textarea name="message" cols="30" rows="10"></textarea>
+          </form>
+      </div>
+    </section>
   )
 }
