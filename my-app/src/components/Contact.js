7b35f852d9d241f51ff7b30ccd79e3c1da5586c7
Eric Emery <ericemery099@gmail.com>
2023-09-05 17:35:30 +0000

    updated email service

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import {useRef} from 'react'
 
 export default function Contact() {
+  const form = useRef()
+
+  const sendEmail = () => {};
   return (
     <section>
       <div className="container">
         <h2 className="text-center">Contact Us</h2>
-          <form>
+          <form ref={form} onSubmit={sendEmail}>
             <input type="text"
             placeholder='Full Name'
             name='user_name' />
@@ -16,6 +20,7 @@ export default function Contact() {
             placeholder='Subject'
             name='subject' required />
             <textarea name="message" cols="30" rows="10"></textarea>
+            <button type="submit" className="btn btn-primary">Send Message</button>
           </form>
       </div>
     </section>
